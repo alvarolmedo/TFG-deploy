3c1f4c129f57e38b93abbc88de6e93df3a0e8b35
Alvaro Olmedo <aolmedo@airongroup.net>
2017-12-12 00:53:12 +0100

    JenkinsUOC doesn't be pushed to registry, because it's not be available at the moment to be builded

--- jenkinsuoc/build.sh
+++ jenkinsuoc/build.sh
@@ -2,11 +2,7 @@
 
 export IMAGE="jenkinsuoc"
 export TAG="latest"
-export REGISTRY="registryuoc:5050"
 
 docker build -t ${IMAGE}:${TAG} ./
 echo "Image Built: $IMAGE Tag: $TAG"
-docker tag ${IMAGE}:${TAG} ${REGISTRY}/${IMAGE}:${TAG}
-docker push ${REGISTRY}/${IMAGE}:${TAG}
-docker rmi jenkinsuoc:latest
 docker rmi jenkins/jenkins:lts
